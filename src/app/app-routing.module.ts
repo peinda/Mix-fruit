15ec39828d92dffeed79c603cf21efb1a33c7d53
peinda <mamependa2011@gmail.com>
2021-05-26 13:35:12 +0000

    panier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'verification-code',
+    redirectTo: 'main',
     pathMatch: 'full'
   },
   {
